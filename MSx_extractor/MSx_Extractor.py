5e37af30ad897c44704eaf30eeee5d4d4013724d
anamdv <76425592+anamdv@users.noreply.github.com>
2022-12-13 09:47:13 +0100

    Add files via upload

---
+++ MSx_extractor/MSx_Extractor.py
@@ -0,0 +1,88 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter.ttk import *
+from tkinter.messagebox import showinfo
+import sys
+import time
+import os
+from pathlib import Path
+from pymsfilereader import MSFileReader
+
+def open_txt():
+	global txt_filename
+	w.filename=filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(("txt files", "*.txt"),))
+	txt_filename=w.filename
+	my_label=Label(w, text=txt_filename).grid(row=0, column=2)
+
+def open_folder():
+	global dir_name
+	w.dirname=filedialog.askdirectory()
+	dir_name=w.dirname
+	my_label=Label(w, text=dir_name).grid(row=1, column=2)
+
+def MSx_extract(dir, file):
+	my_label=Label(w, text="Starting analysis...").grid(row=2, column=2)
+	n=1
+	with open(file) as d:
+		pairs = [tuple(map(float, i.split(','))) for i in d]
+	mylist=[]
+	for filename in os.listdir(dir):
+		if filename.endswith(".raw"):
+			mylist.append(filename)
+		w.update()
+	for filename in mylist:
+		my_label=Label(w, text="Analysing sample: "+str(n)+"/"+str(len(mylist))).grid(row=2, column=2)
+		w.update()
+		os.chdir(dir)
+		rawfile = MSFileReader(filename)
+		ScanNumber=[]
+		with open("%s.results.txt" % filename, "w") as f:
+				sys.stdout=f
+				
+				for x in pairs:
+					os.chdir(dir)
+					#ScanNumber=[]
+					for i in range(1,rawfile.GetNumSpectra()+1):
+						if x==rawfile.GetAllMSOrderData(i)[0][0]:
+								ScanNumber.append(i)
+								print(x,"\t",i ,"\t",rawfile.GetTrailerExtraForScanNum(i)['Multi Inject Info'])
+		n+=1
+		f.close()
+		ScanNumber.sort()
+		scanNumber_to_string=' '.join([str(item) for item in ScanNumber])
+		#print(ScanNumber)
+		with open("%s.config.txt" % filename, "w") as f2:
+			f2.write("filter=\"scanNumber ")
+			f2.write(scanNumber_to_string)
+			f2.write("\"\n")
+			f2.write("outdir="+dir)
+		f2.close()
+		command1='msconvert '+dir+'\\'+filename
+		command2=' -c '+dir+'\\'+filename+'.config.txt'
+		os.chdir("C:\\Users\\svg993\\AppData\\Local\\Apps\\ProteoWizard 3.0.21246.16b8e05d 64-bit")
+		os.system(command1+command2)
+
+
+
+	w.update()
+	my_label=Label(w, text="Done!").grid(row=3, column=2)
+	
+
+
+
+def doMSxExtract():
+	MSx_extract(dir_name, txt_filename)
+
+
+#if __name__ == "__main__":
+import tkinter as tk
+w = Tk()
+w.title("MSx extractor")
+lbl2 = Label(w, text="Upload the IS ENDO file")
+lbl2.grid(row=0, column=0)
+my_btn=Button(w, text="Open File", command=open_txt).grid(row=0, column=1)
+lbl = Label(w, text="Indicate the folder that contains the .raw files")
+lbl.grid(row=1, column=0)
+my_btn=Button(w, text="Select Dir", command=open_folder).grid(row=1, column=1)
+Button(w, text="Go!", command=doMSxExtract).grid(row=2, column=0)
+w.mainloop()
